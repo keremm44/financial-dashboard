9b154a19ab654158cc8c50c8992094a637095656
keremm44 <keremm.4351@gmail.com>
2026-08-19 21:08:05 +0300

    export integrated pattern compression engine

--- src/financial_dashboard/engines/__init__.py
+++ src/financial_dashboard/engines/__init__.py
@@ -1,5 +1,6 @@
 """Stateful analysis-engine interfaces and shared result models."""
 
 from .market_structure_engine import MarketStructureEngine
+from .pattern_compression_engine import PatternCompressionEngine
 
-__all__ = ["MarketStructureEngine"]
+__all__ = ["MarketStructureEngine", "PatternCompressionEngine"]
