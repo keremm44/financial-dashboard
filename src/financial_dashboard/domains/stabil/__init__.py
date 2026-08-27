d0819bab2618f3cc9381baca0ca641a9f61e8fdc
keremm44 <keremm.4351@gmail.com>
2026-08-27 14:27:21 +0300

    refactor: add stabil domain namespace

---
+++ src/financial_dashboard/domains/stabil/__init__.py
@@ -0,0 +1,8 @@
+"""STABIL domain public namespace."""
+
+from ...engines.stabil_support_behavior import *
+from ...engines.stabil_support_lifecycle import *
+from ...engines.stabil_trend_engine import *
+from ...engines.stabil_trend_final import *
+from ...engines.stabil_trend_public import *
+from ...engines.stabil_trend_runtime import *
