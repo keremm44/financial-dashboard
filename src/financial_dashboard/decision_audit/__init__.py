017ab3df8a55aed081eeb3b82e28c9aa80607cdd
keremm44 <keremm.4351@gmail.com>
2026-08-27 19:59:17 +0300

    Turn 10B: export horizon-aware trade quality audit API

--- src/financial_dashboard/decision_audit/__init__.py
+++ src/financial_dashboard/decision_audit/__init__.py
@@ -12,7 +12,16 @@ from .models import (
     SignalStabilityAudit,
     TradeAudit,
 )
+from .quality_reporting import render_trade_quality_json, render_trade_quality_text
 from .reporting import render_json, render_text
+from .trade_quality import (
+    HorizonAwareTradeQualityReport,
+    HorizonTradeQuality,
+    ShortTargetHit,
+    TradeQualityAggregate,
+    TradeQualityAuditConfig,
+    audit_trade_quality,
+)
 
 __all__ = [
     "AggregateTradeMetrics",
@@ -22,11 +31,19 @@ __all__ = [
     "DecisionAuditReport",
     "DecisionEvent",
     "DecisionSide",
+    "HorizonAwareTradeQualityReport",
+    "HorizonTradeQuality",
     "LifecycleAudit",
     "MissedOpportunity",
+    "ShortTargetHit",
     "SignalStabilityAudit",
     "TradeAudit",
+    "TradeQualityAggregate",
+    "TradeQualityAuditConfig",
     "audit_decisions",
+    "audit_trade_quality",
     "render_json",
     "render_text",
+    "render_trade_quality_json",
+    "render_trade_quality_text",
 ]
