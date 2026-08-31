55af789953458f0b9dc946cf736b4fa3f623fba3
keremm44 <keremm.4351@gmail.com>
2026-08-31 22:55:10 +0300

    Remove BUY decision compatibility shells

--- src/financial_dashboard/decision/buy/engine.py
+++
@@ -1,9 +0,0 @@
-"""BUY compatibility facade for the canonical decision engine."""
-
-from ..engine import DecisionEngineConfig, HorizonDecisionAssessment, assess_horizon_decision
-
-__all__ = [
-    "DecisionEngineConfig",
-    "HorizonDecisionAssessment",
-    "assess_horizon_decision",
-]
