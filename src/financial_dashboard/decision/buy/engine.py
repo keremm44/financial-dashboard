d4749ddd2c0be92a4804d460c1baba9b5c9b0c85
keremm44 <keremm.4351@gmail.com>
2026-08-31 20:45:03 +0300

    Make BUY engine facade explicit

--- src/financial_dashboard/decision/buy/engine.py
+++ src/financial_dashboard/decision/buy/engine.py
@@ -1,1 +1,9 @@
-from ..engine import *
+"""BUY compatibility facade for the canonical decision engine."""
+
+from ..engine import DecisionEngineConfig, HorizonDecisionAssessment, assess_horizon_decision
+
+__all__ = [
+    "DecisionEngineConfig",
+    "HorizonDecisionAssessment",
+    "assess_horizon_decision",
+]
